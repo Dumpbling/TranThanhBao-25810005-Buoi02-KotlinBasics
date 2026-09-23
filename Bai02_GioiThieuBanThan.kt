25b6825852a6b061b6ed19a3a6e4c18e9fc5cf08
T-Bao <tranthanhbao161102@gmail.com>
2026-09-23 23:19:26 +0700

    Buoi02 Bai02 gioithieubanthan

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,12 @@
+// TranThanhBao - 25810005
+
+fun main() {
+    val hoTen = "Trần Thanh Bảo"
+    val namSinh = 2002
+    val namHienTai = 2026
+    val tuoi = namHienTai - namSinh
+
+    println("Xin chào, tôi là $hoTen, sinh năm $namSinh, năm nay tôi $tuoi tuổi.")
+
+    println("Kiểm tra: $namHienTai - $namSinh = ${namHienTai - namSinh} tuổi.")
+}
