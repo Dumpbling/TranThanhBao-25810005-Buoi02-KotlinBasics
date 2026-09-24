9aa3aaa4ed9d2d14909037f298917a7000e1f12f
Dumpbling <tranthanhbao161102@gmail.com>
2026-09-24 14:41:01 +0700

    Update introduction message format

--- Bai02_GioiThieuBanThan.kt
+++ Bai02_GioiThieuBanThan.kt
@@ -6,7 +6,7 @@ fun main() {
     val namHienTai = 2026
     val tuoi = namHienTai - namSinh
 
-    println("Xin chào, tôi là $hoTen, sinh năm $namSinh, năm nay tôi $tuoi tuổi.")
+    println("Xin chào thầy và các bạn,em là $hoTen, sinh năm $namSinh, năm nay em $tuoi tuổi.")
 
     println("Kiểm tra: $namHienTai - $namSinh = ${namHienTai - namSinh} tuổi.")
 }
