f2c5adac39f74cc8ab8485be33995b54525ed0f0
T-Bao <tranthanhbao161102@gmail.com>
2026-09-26 18:51:47 +0700

    Bai02: Tinh dien tich

---
+++ TranThanhBao_25810005_Buoi03_KotlinBasic/Bai02_Buoi03_TinhDienTich.kt
@@ -0,0 +1,9 @@
+//TranThanhBao_25810005
+fun tinhDienTich(chieuDai: Double, chieuRong: Double): Double {
+    return chieuDai * chieuRong
+}
+
+fun main() {
+    println(tinhDienTich(5.0, 3.0))
+    println(tinhDienTich(7.5, 2.5))
+}
