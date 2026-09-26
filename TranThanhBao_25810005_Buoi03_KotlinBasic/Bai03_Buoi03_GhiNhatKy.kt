d18d589d6771b6c3d1637ed990a2defe903915c7
T-Bao <tranthanhbao161102@gmail.com>
2026-09-26 18:52:02 +0700

    Bai03: Ghi nhat ky

---
+++ TranThanhBao_25810005_Buoi03_KotlinBasic/Bai03_Buoi03_GhiNhatKy.kt
@@ -0,0 +1,13 @@
+//TranThanhBao_25810005
+fun ghiNhatKy(hanhDong: String): Unit {
+    println("[LOG] $hanhDong")
+}
+
+fun ghiNhatKyRutGon(hanhDong: String) {
+    println("[LOG] $hanhDong")
+}
+
+fun main() {
+    ghiNhatKy("Dang nhap he thong")
+    ghiNhatKyRutGon("Dang nhap he thong")
+}
